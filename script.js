e54e19a666711d1ea8aaec01c58ea585a6ab5609
Kevsely <elegis.sossou@yahoo.com>
2022-05-11 05:42:37 +0100

    Fix a bug in update item

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ let listArrays = []
 // Drag Functionality
 let draggedItem
 let currentColumn
+let dragging = false
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -112,12 +113,14 @@ function updateDOM() {
 function updateItem(id, column) {
   const selectedArray = listArrays[column]
   const selectedColumnEl = listColumns[column].children
-  if (!selectedColumnEl[id].textContent) {
-    delete selectedArray[id]
-  } else {
-    selectedArray[id] = selectedColumnEl[id].textContent
+  if (!dragging) {
+    if (!selectedColumnEl[id].textContent) {
+      delete selectedArray[id]
+    } else {
+      selectedArray[id] = selectedColumnEl[id].textContent
+    }
+    updateDOM()
   }
-  updateDOM()
 }
 
 // Add to Column List, Reset Textbox
@@ -168,6 +171,7 @@ function rebuildArrays() {
 // When Item Starts Dragging
 function drag(e) {
   draggedItem = e.target
+  dragging = true
 }
 
 // Column Allows for Item to Drop
@@ -191,6 +195,7 @@ function drop(e) {
   // Add Item to Column
   const parent = listColumns[currentColumn]
   parent.appendChild(draggedItem)
+  dragging = false
   rebuildArrays
 }
 
